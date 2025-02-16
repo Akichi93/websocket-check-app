8cf18280ef4a6fa676c04ae9bf222853bb965302
Akichi93 <jeanphilippeakichi@gmail.com>
2025-02-16 21:38:04 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+// server.js
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+// Activer CORS pour les requêtes HTTP
+app.use(cors({
+  origin: "http://localhost:5173", // Adresse de votre frontend Vue.js
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+
+// Configurer CORS pour Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Adresse de votre frontend Vue.js
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+let connectedUsers = {}; // Stocker les utilisateurs connectés
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// Gérer les connexions Socket.IO
+io.on("connection", (socket) => {
+  console.log("Un utilisateur est connecté:", socket.id);
+
+  // Ajouter l'utilisateur à la liste des connectés
+  connectedUsers[socket.id] = { connected: true };
+
+  // Informer tous les clients de l'état des connexions
+  io.emit("user-status-update", connectedUsers);
+
+  // Gérer la déconnexion
+  socket.on("disconnect", () => {
+    console.log("Un utilisateur est déconnecté:", socket.id);
+
+    // Retirer l'utilisateur de la liste des connectés
+    delete connectedUsers[socket.id];
+    io.emit("user-status-update", connectedUsers);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Le serveur est en écoute sur le port 3000");
+});
