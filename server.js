8ab018c7ddc2cfb0e8fb8d7d251b67272b54687a
Akichi93 <59120694+Akichi93@users.noreply.github.com>
2025-03-07 11:59:55 +0000

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
     credentials: true,
   },
-  transports: ["websocket"], // Forcer l'utilisation de WebSocket
+ transports: ["websocket", "polling"],
 });
 
 let connectedUsers = {}; // Stocker les utilisateurs connectés
