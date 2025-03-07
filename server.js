c8153860761b270fb350d623dc3e09b2ee4d882a
Akichi93 <59120694+Akichi93@users.noreply.github.com>
2025-03-07 13:25:17 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,8 @@ const app = express();
 const server = http.createServer(app);
 
 // Activer CORS pour les requêtes HTTP
-const allowedOrigins = ["https://courtage.fl4ir.com", "http://localhost:5173"];
 app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Origine non autorisée par CORS"));
-    }
-  },
+  origin: "https://courtage.fl4ir.com", // Adresse de votre frontend Vue.js
   methods: ["GET", "POST"],
   credentials: true,
 }));
@@ -23,11 +16,10 @@ app.use(cors({
 // Configurer CORS pour Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: allowedOrigins,
+    origin: "https://courtage.fl4ir.com", // Adresse de votre frontend Vue.js
     methods: ["GET", "POST"],
     credentials: true,
   },
- transports: ["websocket", "polling"],
 });
 
 let connectedUsers = {}; // Stocker les utilisateurs connectés
@@ -40,23 +32,22 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("Un utilisateur est connecté:", socket.id);
 
-  // Gérer les erreurs de connexion
-  socket.on("connect_error", (err) => {
-    console.error("Erreur de connexion Socket.IO:", err.message);
-  });
-
   // Ajouter l'utilisateur à la liste des connectés
   connectedUsers[socket.id] = { connected: true };
+
+  // Informer tous les clients de l'état des connexions
   io.emit("user-status-update", connectedUsers);
 
   // Gérer la déconnexion
   socket.on("disconnect", () => {
     console.log("Un utilisateur est déconnecté:", socket.id);
+
+    // Retirer l'utilisateur de la liste des connectés
     delete connectedUsers[socket.id];
     io.emit("user-status-update", connectedUsers);
   });
 });
 
 server.listen(3000, () => {
-  console.log("Le serveur est en écoute sur le port 3000");
+  console.log("Le serveur est en écoute sur le port 3000");
 });
